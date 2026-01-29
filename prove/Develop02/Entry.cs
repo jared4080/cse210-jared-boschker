1c3ad62f24ae57fa3d6afa60f6ec85bbfb78d58a
Jared Boschker <jared.boschker@gmail.com>
2026-01-29 14:51:44 -0700

    started on entry class creation

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Develop02
+{
+    public class Entry
+    {
+        List<string> prompts = new List<string>{"what?", "hahahahh", "lol haha", "sha", "baka"};
+        string _prompt = "";
+        string _text_entry = "";
+
+        public Entry(string: prompt, string: text_entry){
+            _prompt = _prompt;
+            _text_entry = text_entry;
+            DateTime date = DateTime.Now();
+        }
+    }
+}
