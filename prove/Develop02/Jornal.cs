2487642a2897b8bddec11055148e534767f1628e
Jared Boschker <jared.boschker@gmail.com>
2026-01-29 16:07:24 -0700

    created write entry function

--- prove/Develop02/Jornal.cs
+++ prove/Develop02/Jornal.cs
@@ -1,11 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks.Dataflow;
 
 
 namespace Develop02
 {
     public class Journal
     {
-        
+        void WriteEntry()
+        {
+            Random _rand = new Random();
+
+            string prompt = JournalData.prompts[_rand.Next(JournalData.prompts.Count)];
+
+            Console.WriteLine(prompt);
+            string user_entry = Console.ReadLine();
+
+            Entry journal_entry = new Entry(prompt, user_entry);
+        }
     }
 }
